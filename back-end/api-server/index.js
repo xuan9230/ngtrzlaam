5b4939b04adf450be21aea02d8f1e4a5de5d9cbe
Stan <stanley.luo@bappo.com>
2020-03-17 11:02:58 +1100

    (api): Migrate to express-graphql

---
+++ back-end/api-server/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const { buildSchema } = require("graphql");
+const fs = require("fs");
+const path = require("path");
+
+const schemaDef = fs.readFileSync(path.join(__dirname, "../../schema.graphql"));
+const schema = buildSchema(`${schemaDef}`);
+
+const dummyMessages = [
+  { body: "ee", id: "1", author: "Q" },
+  { body: "asd", id: "2", author: "W" }
+];
+
+const rootValue = {
+  info: () => "hello banana",
+  messages: (args, req) => dummyMessages,
+  message: ({ id }) => dummyMessages.find(m => m.id === id),
+  createMessage: ({ input }) => {
+    const newMessage = {
+      author: input.author,
+      body: input.body,
+      id: `${dummyMessages.length + 1}`
+    };
+    dummyMessages.push(newMessage);
+    return newMessage;
+  }
+};
+
+/**
+ * Logging middleware
+ */
+const loggingMiddleware = (req, res, next) => {
+  console.log("ip:", req.ip);
+  next();
+};
+
+const app = express();
+// app.use(loggingMiddleware);
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true
+  })
+);
+
+app.listen(4000);
+
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
