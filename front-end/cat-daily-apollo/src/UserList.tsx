e585c3fdd9742f64e655d08901bd102b7952dedd
Stan <stanley.luo@bappo.com>
2020-04-05 21:01:29 +1000

    (front-end): add deck and card

--- front-end/cat-daily-apollo/src/UserList.tsx
+++ front-end/cat-daily-apollo/src/UserList.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 
+import Deck from "./Deck";
+
 export default function UserList() {
+  const [selectedUser, setSelectedUser] = useState<null | any>(null);
+
+  if (selectedUser) return <Deck />;
+
   return (
     <List component="nav" aria-label="secondary mailbox folders">
-      <ListItem button>
+      <ListItem button onClick={() => setSelectedUser("Connie")}>
         <ListItemText primary="Connie" />
       </ListItem>
       <ListItem button>
